7d52e8685631009fb26317c7e6d96727435f7351
Ayon95 <mushfiqurrahman78@yahoo.com>
2021-08-11 00:05:19 -0400

    added gatsby-plugin-react-helmet to plugins array in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	plugins: [
 		'gatsby-plugin-styled-components',
+		'gatsby-plugin-react-helmet',
 		'gatsby-plugin-image',
 		'gatsby-plugin-sharp',
 		'gatsby-transformer-sharp',
